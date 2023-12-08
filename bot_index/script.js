6ba988a97990f559cf8697882c26b801cbfe915e
Alex <iamkidandyouknowit@gmail.com>
2023-12-08 12:34:54 +0500

    Design changes

--- bot_index/script.js
+++ bot_index/script.js
@@ -1,42 +1,17 @@
-function Move(obj, max_l, min_l, max_t, min_t){
-    var object = document.getElementById(obj);
-    var position_l = Math.random() * (max_l - min_l) + min_l;
-    var position_t = Math.random() * (max_t - min_t) + min_t;
-    object.style.left = position_l + "px";
-    object.style.top = position_t + "px";
-}
+function showContent(contentId) {
+    var aDiv = document.getElementById('a');
+    var bDiv = document.getElementById('b');
+    var cDiv = document.getElementById('c');
 
-function aShow(){
-    var a = document.getElementById("a");
-    var b = document.getElementById("b");
-    var c = document.getElementById("c");
-    a.style.visibility = "visible";
-    b.style.visibility = "hidden";
-    c.style.visibility = "hidden";
-}
+    aDiv.classList.remove('active');
+    bDiv.classList.remove('active');
+    cDiv.classList.remove('active');
 
-function bShow(){
-    var a = document.getElementById("a");
-    var b = document.getElementById("b");
-    var c = document.getElementById("c");
-    a.style.visibility = "hidden";
-    b.style.visibility = "visible";
-    c.style.visibility = "hidden";
+    if (contentId === 'a') {
+        aDiv.classList.add('active');
+    } else if (contentId === 'b') {
+        bDiv.classList.add('active');
+    } else if (contentId === 'c') {
+        cDiv.classList.add('active');
+    }
 }
-
-function cShow(){
-    var a = document.getElementById("a");
-    var b = document.getElementById("b");
-    var c = document.getElementById("c");
-    a.style.visibility = "hidden";
-    b.style.visibility = "hidden";
-    c.style.visibility = "visible";
-}
-
-function main(){
-    setTimeout(function(){
-        document.body.classList.add('body_visible');
-    }, 1);
-}
-
-main();
